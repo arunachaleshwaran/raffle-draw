489f8a9574af0ca2fc580bb17f5b8efd9fa8ca32
arunachaleshwaran <arunachaleshwaran12168@gmail.com>
2025-05-26 01:32:39 +0530

    ✨show brand name instead of name

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,12 @@ import { useMemo, useRef, useState } from 'react';
 import { ScratchToReveal } from '@/components/magicui/scratch-to-reveal';
 import { AnimatedList } from '@/components/magicui/animated-list';
 import { PriceCard } from './components/price-card';
-
+import BrandVsPeople from './brand-vs-people.json';
 function App() {
   const [state, setState] = useState<
     'button' | 'button-click' | 'spin-wheel' | 'winner' | 'price-revile'
   >('button');
-  const [winners, setWinners] = useState<Array<{ name: string; prize: string }>>([]);
+  const [winners, setWinners] = useState<Array<{ brand: string; prize: number; name: string }>>([]);
   const homeSpanRef = useRef<HTMLSpanElement>(null);
   const priceCardRef = useRef<HTMLDivElement>(null);
   const { trigger, isRunning } = useConfetti();
@@ -41,7 +41,36 @@ function App() {
       priceCardRef.current.classList.remove('w-full', 'h-full');
     }
   }, [isClicking]);
-  const prizes = ['500', '1000', '2000'];
+  const prizes: Array<{ amount: number; title: React.ReactNode }> = [
+    {
+      amount: 500,
+      title: (
+        <>
+          3<sup>rd</sup> Prize: Pit Stop
+        </>
+      ),
+    },
+    {
+      amount: 1000,
+      title: (
+        <>
+          2<sup>nd</sup> Prize: Turbocharged
+        </>
+      ),
+    },
+    {
+      amount: 2000,
+      title: (
+        <>
+          1<sup>st</sup> Prize: Pole Position
+        </>
+      ),
+    },
+  ];
+  const currentPriceIndex = winners.filter((winner) => winner.prize).length;
+  const prizeDetail = useMemo(() => {
+    return prizes[currentPriceIndex];
+  }, [currentPriceIndex]);
   const segments = [
     'Trust',
     'Transparency',
@@ -72,10 +101,11 @@ function App() {
     rootEle.style.removeProperty('animation-duration');
     rootEle.style.removeProperty('background-blend-mode');
   };
-  const onFinished = (winner: string) => {
+  const onFinished = () => {
     trigger();
     setTimeout(() => setState('winner'), 5000);
-    setWinners((state) => [...state, { name: winner, prize: '' }]);
+    const winner = BrandVsPeople[Math.floor(Math.random() * BrandVsPeople.length)];
+    setWinners((state) => [...state, { brand: winner.brand, prize: 0, name: winner.name }]);
     priceCardRef.current?.classList.remove('button-animate');
     removeRootStyle();
   };
@@ -96,12 +126,8 @@ function App() {
           {(state == 'button' || state == 'button-click') && (
             <PriceCard
               ref={priceCardRef}
-              price={500}
-              title={
-                <>
-                  3<sup>rd</sup> Prize Pit Stop
-                </>
-              }
+              price={prizeDetail.amount}
+              title={prizeDetail.title}
               color="13551f"
               className="h-3/12 w-2/3  select-none button-animate rounded-full "
               {...longPressEvent}
@@ -134,14 +160,14 @@ function App() {
                 trigger();
                 setWinners((state) => {
                   const newWinners = [...state];
-                  newWinners[newWinners.length - 1].prize = prizes[winners.length - 1];
+                  newWinners[newWinners.length - 1].prize = prizes[winners.length - 1].amount;
                   setState('price-revile');
                   return newWinners;
                 });
               }}
             >
               <span className="bg-secondary-background border-8 border-dashed w-full h-full flex justify-center items-center rounded-[20%] text-6xl">
-                {prizes[winners.length - 1]}
+                {winners[winners.length - 1].brand}
               </span>
             </ScratchToReveal>
           )}
@@ -163,7 +189,7 @@ function App() {
               key={winner.prize}
               className="bg-card-background rounded-lg p-4 flex flex-col items-center justify-center"
             >
-              <h3 className="text-xl7 font-bold ">{winner.name}</h3>
+              <h3 className="text-xl7 font-bold ">{winner.brand}</h3>
               <p className="text-lg">{winner.prize}</p>
             </div>
           ))}
