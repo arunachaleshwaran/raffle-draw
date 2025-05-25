f31cb81787c01404097c76443201f5389c48ed04
arunachaleshwaran <arunachaleshwaran12168@gmail.com>
2025-05-25 23:13:37 +0530

    ✨change segments details

--- src/App.tsx
+++ src/App.tsx
@@ -15,24 +15,28 @@ function App() {
   const homeSpanRef = useRef<HTMLSpanElement>(null);
   const { trigger, isRunning } = useConfetti();
   const segments = [
-    'better luck next time',
-    'won 70',
-    'won 10',
-    'better luck next time',
-    'won 2',
-    'won uber pass',
-    'better luck next time',
-    'won a voucher',
+    'Trust',
+    'Transparency',
+    'People First',
+    'Empathy',
+    'Persistence',
+    'Empowerment',
+    'Respect',
+    'Listen',
+    'Resilience',
+    'Evolve',
   ];
   const segColors = [
-    '#EE4040',
-    '#F0CF50',
-    '#815CD1',
-    '#3DA5E0',
-    '#34A24F',
-    '#F9AA1F',
-    '#EC3F3F',
-    '#FF9000',
+    '#0077FF',
+    '#A7F3D0',
+    '#A4E100',
+    '#00B0B9',
+    '#9B2D9F',
+    '#FFCC00',
+    '#4DB8FF',
+    '#D5006D',
+    '#333333',
+    '#FF6F61',
   ];
   const rootEle = document.getElementById('root')!;
   const removeRootStyle = () => {
